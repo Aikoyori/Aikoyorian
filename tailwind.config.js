08f85634d46fa8c7fcd9a0ef8b881d013e4d9655
Aikoyori <12034280+Aikoyori@users.noreply.github.com>
2022-06-17 03:12:32 +0700

    Initial Commit Part 2 Electric Boogaloo

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./templates/*.{mustache,html,js,php}",
+  "resources/*.{html,js,php}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
